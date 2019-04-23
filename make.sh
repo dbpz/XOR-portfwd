77352ef6b3854e9a075130763b94471af4941792
DBPZ <yang@liao.id.au>
2019-04-23 09:24:55 +0300

    More changes

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-CC="c++ -o 9 "
+CC="c++ -O1 -ggdb "
 rm -f *.o
 for f in `ls *[.]cc |grep -v portfwd.cc|grep -v lex.yy`; do t=`echo $f |sed 's/.cc$//g'`; $CC -o $t.o -c $f   -DPORTFWD_CONF=\"\" ; done
 for f in `ls *[.]c| grep -v lex.yy`; do t=`echo $f |sed 's/[.]c$//g'`; $CC -o $t.o -c $f   -DPORTFWD_CONF=\"\" ; done
